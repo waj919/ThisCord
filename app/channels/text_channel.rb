6aff7934a7e3e23dd994714fcb79d85659e3eaae
Wajahat Shoukat <wajahatshoukat@gmail.com>
2021-09-15 22:05:10 -0400

    working on websockets

---
+++ app/channels/text_channel.rb
@@ -0,0 +1,14 @@
+class TextChannel < ApplicationCable::TextChannel
+
+   # def subscribed
+        #@channel = Channel.find_by(id: param[:id])
+
+  #      stream_for @channel
+  #  end
+
+   # def receive(data)
+  #      message = Message.create(body: data['message]'], channel_id: @channel.id)
+    #    TextChannel.broadcast_to(@channel, message: data['message]'])
+    #end
+
+##end
